df749db5c36dda1e8f4d526707d65501f4748db8
tn <prtnog@gmail.com>
2023-12-25 03:08:26 +0900

    discarded supertest

--- test/app/app.test.ts
+++ test/app/app.test.ts
@@ -1,12 +1,17 @@
+import axios, { CreateAxiosDefaults, AxiosResponse } from 'axios';
 import * as cheerio from 'cheerio';
-import setCookie from 'set-cookie-parser';
-import supertest from 'supertest';
 import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
 import { createApp, defaultConfig } from '../../src/app/app.js';
 import { createTestData } from '../../src/app/test-data.js';
 import { User } from '../../src/id/models/user.js';
 import { Users } from '../../src/id/repositories/users.js';
+import { AddressInfo } from 'net';
+
+const axiosCfg: CreateAxiosDefaults = {
+  validateStatus: () => true, // Don't throw HTTP exceptions.
+  maxRedirects: 0, // No redirects.
+};
 
 const cfg = defaultConfig;
 const users = new Users();
@@ -20,7 +25,9 @@ beforeAll(async () => {
   app = await createApp(false, cfg, users);
   testData = await createTestData(users);
 
-  await app.ready();
+  await app.listen(); // Use randomized port in tests.
+  const address = app.server.address() as AddressInfo;
+  axiosCfg.baseURL = `http://127.0.0.1:${address.port}`;
 });
 
 afterAll(() => {
@@ -29,18 +36,18 @@ afterAll(() => {
 
 describe('GET /signin', () => {
   it('returns the signin page', async () => {
-    const agent = supertest.agent(app.server);
+    const agent = axios.create(axiosCfg);
 
     const res = await agent.get('/signin');
     expect(res.status).toBe(200);
-    expect(res.text).toMatch('username');
-    expect(res.text).toMatch('csrf');
+    expect(res.data).toMatch('username');
+    expect(res.data).toMatch('csrf');
   });
 });
 
 describe('GET /home', () => {
   it('redirects to the signin page (unauthenticated)', async () => {
-    const agent = supertest.agent(app.server);
+    const agent = axios.create(axiosCfg);
 
     const res = await agent.get('/home');
     expect(res.status).toBe(302);
@@ -48,34 +55,47 @@ describe('GET /home', () => {
   });
 
   it('returns the home page (authenticated)', async () => {
-    const agent = supertest.agent(app.server);
+    const agent = axios.create(axiosCfg);
 
     let res = await agent.get('/home');
     expect(res.status).toBe(302);
     let location = res.headers.location;
     expect(location).toEqual('/signin');
-    let sCookies = setCookie(res.headers['set-cookie']);
-    let cookies = sCookies.map((c) => `${c.name}=${c.value}`).join('; ');
+    let cookie = getSessionCookie(res);
 
-    res = await agent.get(location).set('cookie', cookies);
+    res = await agent.get(location, { headers: { Cookie: cookie } });
     expect(res.status).toBe(200);
-    const dollar = cheerio.load(res.text);
+    const dollar = cheerio.load(res.data);
     const csrfToken = dollar('#signin-csrf').attr('value');
 
-    res = await agent
-      .post(location)
-      .set('cookie', cookies)
-      .send(`username=${testData.users[0].userName}`)
-      .send(`password=${testData.userPasswords[0]}`)
-      .send(`_csrf=${csrfToken}`);
+    const params = new URLSearchParams({
+      username: testData.users[0].userName,
+      password: testData.userPasswords[0],
+      _csrf: csrfToken!,
+    });
+    res = await agent.post(location, params, { headers: { Cookie: cookie } });
     expect(res.status).toBe(302);
     location = res.headers.location;
     expect(location).toEqual('/home');
-    sCookies = setCookie(res.headers['set-cookie']);
-    cookies = sCookies.map((c) => `${c.name}=${c.value}`).join('; ');
+    cookie = getSessionCookie(res);
 
-    res = await agent.get(location).set('cookie', cookies);
+    res = await agent.get(location, { headers: { Cookie: cookie } });
     expect(res.status).toBe(200);
-    expect(res.text).toMatch(`User name: ${testData.users[0].userName}`);
+    expect(res.data).toMatch(`User name: ${testData.users[0].userName}`);
   });
 });
+
+
+function getSessionCookie(res: AxiosResponse) {
+  const setCookie = res.headers['set-cookie'];
+  if (!setCookie) {
+    return '';
+  }
+
+  const sessionCookie = setCookie.find(cookie => cookie.match(/^app-sessionid=[a-zA-Z0-9]/));
+  if (!sessionCookie) {
+    return '';
+  }
+
+  return sessionCookie.replace(/;.*/, '');
+}
